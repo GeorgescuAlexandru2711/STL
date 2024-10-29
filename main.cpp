b1ccd8a80694541a6a03b62a99930d433745a8d9
GeorgescuAlexandru2711 <georgescu.alexandru.c4v@student.ucv.ro>
2024-10-29 18:01:36 +0200

    my problem

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
 
 int main()
 {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
+    int no_problems, no_doctors, no_accept;
+    string name, problem_speciality, doctor_speciality;
     inFile >> no_problems;
+    vector<string>id_probleme(no_problems);
+    vector<string>specializare_problema(no_problems);
 
-    for (int i = 0; i < no_problems; i++)
+    for (int i=0;i<no_problems;i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> id_probleme[i];
+        inFile >> specializare_problema[i];
     }
 
     inFile >> no_doctors;
 
-    for (int i = 0; i < no_doctors; i++)
+    vector<string>id_doctor(no_doctors);
+    vector<string>specializare_doctor(no_doctors);
+    for (int i=0;i<no_doctors;i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> id_doctor[i];
+        inFile >> specializare_problema[i];
     }
 
+    for (int i = 0; i < no_problems; i++) {
+        for (int j = 0; j < no_doctors; j++) {
+            if (specializare_problema[i] == specializare_doctor[j]) {
+                cout << specializare_problema[i] << " " << "Acceptat";
+                cout << endl;
+            }
+        }
+    }
+    
     return 0;
 }
